71db8d95f880bb554d1ab4e440a01ecb91910b21
Octavian Sandu <soctavianstefan@gmail.com>
2024-03-29 12:30:41 +0200

    kdeconnect

--- configuration.nix
+++ configuration.nix
@@ -319,6 +319,15 @@ environment.sessionVariables = {
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
+   networking.firewall = { 
+    enable = true;
+    allowedTCPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+    allowedUDPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+  }; 
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
