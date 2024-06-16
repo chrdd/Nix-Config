3644c61d67cc8fd2baf12611d01c56c7c9d63bed
Octavian Sandu <soctavianstefan@gmail.com>
2024-06-16 13:51:28 +0300

    After restructure - broken

--- configuration.nix
+++ configuration.nix
@@ -1,24 +1,18 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-
 { inputs, config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      #./apps
-      inputs.home-manager.nixosModules.home-manager
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.home-manager
+  ];
 
   nix = {
-  package = pkgs.nixFlakes;
-  extraOptions = ''experimental-features = nix-command flakes'';
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
   };
-  
-  #Home-manager
+
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
     users = {
@@ -26,12 +20,7 @@
     };
   };
 
-
-  # Bootloader.
-  #boot.loader.grub.efiSupport = true;
-  #boot.loader.grub.useOSProber = true;
- 
-    boot = {
+  boot = {
     tmp.cleanOnBoot = true;
     supportedFilesystems = ["ntfs"];
     loader = {
@@ -47,20 +36,10 @@
     };
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  #networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "Europe/Bucharest";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -74,84 +53,76 @@
     LC_TELEPHONE = "ro_RO.UTF-8";
     LC_TIME = "ro_RO.UTF-8";
   };
-# Enable Hyprland
-programs.hyprland = {
-  enable = true;
-  package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-  xwayland.enable = true;
-};
 
-
-programs.thunar.enable = true;
-programs.thunar.plugins = with pkgs.xfce; [
-  thunar-archive-plugin
-  thunar-volman
-];
-
-services.gvfs.enable = true; # Mount, trash, and other functionalities
-services.tumbler.enable = true; # Thumbnail support for images
-programs.file-roller.enable = true;
-
-#Bluetooth
-hardware.bluetooth.enable = true; # enables support for Bluetooth
-hardware.bluetooth.powerOnBoot = true; 
-services.blueman.enable = true;
-
-
-
-# Flatpak
-# https://github.com/gmodena/nix-flatpak
-services.flatpak.enable = true;
-services.flatpak.packages = [
-  { appId = "tv.plex.PlexDesktop"; origin = "flathub"; }
-  { appId = "tv.plex.PlexHTPC"; origin = "flathub"; }
-  { appId = "com.github.tchx84.Flatseal"; origin = "flathub"; }
-];
-
-
-# Tailscale
-services.tailscale.enable = true;
-
-# Media keys
-services.actkbd = {
+  programs.hyprland = {
     enable = true;
-    bindings = [
-    { keys = [ 123 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/runuser -l octavian -c 'amixer set Master 5%+'"; }
-    { keys = [ 122 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/runuser -l octavian -c 'amixer set Master 5%-'"; }
-    { keys = [ 121 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/runuser -l octavian -c 'amixer -D pulse set Master 1+ toggle'"; }
-  ];
-};
-
-
-# Steam
-programs.steam={
-  enable = true;
-  remotePlay.openFirewall = true;  
-};
-
-programs.zsh = {
-  enable = true;
-  enableCompletion = true;
-  autosuggestions.enable = true;
-  syntaxHighlighting.enable = true;
-  ohMyZsh = {
-        enable = true;
-        plugins = [ "git" ];
-        theme="eastwood";
-    }; 
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    xwayland.enable = true;
   };
 
-environment.sessionVariables = {
-  WLR_NO_HARDWARE_CURSORS = "1";
-  NIXOS_OZONE_WL = "1";
-  LIBGL_DEBUG = "verbose";
-  EGL_PLATFORM = "wayland";
-};
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+  };
 
- 
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+  programs.file-roller.enable = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = true;
+
+  services.flatpak.enable = true;
+  services.flatpak.packages = [
+    { appId = "tv.plex.PlexDesktop"; origin = "flathub"; }
+    { appId = "tv.plex.PlexHTPC"; origin = "flathub"; }
+    { appId = "com.github.tchx84.Flatseal"; origin = "flathub"; }
+  ];
+
+  services.tailscale.enable = true;
+
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [123]; events = ["key"]; command = "/run/current-system/sw/bin/runuser -l octavian -c 'amixer set Master 5%+'"; }
+      { keys = [122]; events = ["key"]; command = "/run/current-system/sw/bin/runuser -l octavian -c 'amixer set Master 5%-'"; }
+      { keys = [121]; events = ["key"]; command = "/run/current-system/sw/bin/runuser -l octavian -c 'amixer -D pulse set Master 1+ toggle'"; }
+    ];
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = ["git"];
+      theme = "eastwood";
+    };
+  };
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+    LIBGL_DEBUG = "verbose";
+    EGL_PLATFORM = "wayland";
+  };
+
   services.xserver.videoDrivers = ["nvidia"];
 
-  # Nvidia settings
   hardware.nvidia = {
     modesetting.enable = true;
     nvidiaSettings = true;
@@ -160,103 +131,56 @@ environment.sessionVariables = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  # services.xserver.videoDrivers = [ "nvidia" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
 
-
- 
-    #OBS
-    # https://nixos.wiki/wiki/OBS_Studio
-   #boot.extraModulePackages = with config.boot.kernelPackages; [
-   #  v4l2loopback
-   #];
-   #boot.kernelModules = [“v4l2loopback”];
-    boot.extraModulePackages = with config.boot.kernelPackages;
-    [ v4l2loopback.out ];
-
-    # Activate kernel modules (choose from built-ins and extra ones)
   boot.kernelModules = [
-    # Virtual Camera
     "v4l2loopback"
-    # Virtual Microphone, built-in
     "snd-aloop"
   ];
 
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="Virtual Cam" exclusive_caps=1
+  '';
 
-   boot.extraModprobeConfig = ''
-     options v4l2loopback devices=1 video_nr=1 card_label="Virtual Cam" exclusive_caps=1
-   '';
-   security.polkit.enable = true; 
-   
-   # Autostart polkit_gnome
-   systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
+  security.polkit.enable = true;
+
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
     description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
+    wantedBy = ["graphical-session.target"];
+    wants = ["graphical-session.target"];
+    after = ["graphical-session.target"];
     serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-        };
-     };
-   };
-
-   #Load Nvidia drivers 
-   #services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
-   #services.xserver.displayManager.gdm = {
-     #enable = true;
-     #nvidiaWayland = true;
-   #};  
-   # Enable the X11 windowing system.
-   services.xserver.enable = true;
-   #RDP
-   #services.xserver.displayManager.sddm.enable = true;
-   services.xserver.desktopManager.plasma5.enable = true;
-
-   #services.xrdp.enable = true;
-   #services.xrdp.defaultWindowManager = "hyprland";
-   #services.xrdp.openFirewall = true;
-   # Enable the KDE Plasma Desktop Environment.
-   #services.xserver.displayManager.sddm.wayland.enable = true;
-   services.displayManager.sddm.wayland.enable = true;
-   services.xserver.displayManager.gdm.enable = true; 
-   #Default session
-   services.displayManager.defaultSession = "hyprland";
-   
-   # Sway
-   #programs.sway = {
-   #  enable = true;
-   #  wrapperFeatures.gtk = true;
-   #};
-   # Swaylock
-   #programs.swaylock = {
-   #  enable= true;
-   #};
-   # Swaylock fix
-   security.pam.services.swaylock = {};
-   #services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
-  
-  services.xserver.xkb.variant = "";
-  services.xserver.xkb.layout = "us";
-
-  services = {
-    syncthing = {
-        enable = true;
-        user = "octavian";
-        dataDir = "/home/octavian/Documents";    # Default folder for new synced folders
-        configDir = "/home/octavian/Documents/.config/syncthing";   # Folder for Syncthing's settings and keys
-      };
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
   };
 
-  # Enable CUPS to print documents.
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.displayManager.defaultSession = "hyprland";
+
+  security.pam.services.swaylock = {};
+
+  services.xserver.xkb = {
+    variant = "";
+    layout = "us";
+  };
+
+  services.syncthing = {
+    enable = true;
+    user = "octavian";
+    dataDir = "/home/octavian/Documents";
+    configDir = "/home/octavian/Documents/.config/syncthing";
+  };
+
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -265,57 +189,40 @@ environment.sessionVariables = {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.octavian = {
     isNormalUser = true;
     description = "octavian";
-    extraGroups = [ "networkmanager" "wheel" "audio" ];
+    extraGroups = ["networkmanager" "wheel" "audio"];
     packages = with pkgs; [
       firefox
       kate
-    #  thunderbird
     ];
   };
 
-
-  #docker
-  #virtualisation.docker.enable = true;
-  #users.users.octavian.extraGroups = [ "docker" ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = pkgs.lib.mkForce true;  
+  nixpkgs.config.allowUnfree = pkgs.lib.mkForce true;
   nixpkgs.config.allowInsecure = true;
-  
-  # Vivaldi
-  nixpkgs.config.vivaldi = {
-      proprietaryCodecs = true;
-      enableWideVine = true;
-    };
-  #Sunshine
-  security.wrappers.sunshine = {
-      owner = "root";
-      group = "root";
-      capabilities = "cap_sys_admin+p";
-      source = "${pkgs.sunshine}/bin/sunshine";
-  };
-  
-  services.avahi.enable = true; 
-  services.avahi.publish.enable = true;
-  services.avahi.publish.userServices = true;
 
-  #Fonts
-  #fonts.fontconfig.enableProfileFonts = true;
+  nixpkgs.config.vivaldi = {
+    proprietaryCodecs = true;
+    enableWideVine = true;
+  };
+
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
+
+  services.avahi = {
+    enable = true;
+    publish.enable = true;
+    publish.userServices = true;
+  };
+
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk
@@ -328,21 +235,13 @@ environment.sessionVariables = {
     proggyfonts
     jetbrains-mono
   ];
-  
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     hyprland
-  # cudaPackages.nvidia_driver
     wget
-    pkgs.waybar
-    (pkgs.waybar.overrideAttrs (oldAttrs: {
+    pkgs.waybar.overrideAttrs (oldAttrs: {
       mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
     })
-    )
-    #wl-paste
     discord
     bitwarden
     obsidian
@@ -356,15 +255,12 @@ environment.sessionVariables = {
     zabbixctl
     vivaldi
     docker-compose
-   # modelsim
     youtube-music
-    
     net-snmp
     gparted
     f3
     rpi-imager
     droidcam
-    #v4l2loopback
     postgresql
     postman
     jetbrains.idea-ultimate
@@ -377,7 +273,6 @@ environment.sessionVariables = {
     git
     alacritty
     rofi-wayland
-   # floorp
     libva
     libvdpau
     libva-utils
@@ -409,7 +304,6 @@ environment.sessionVariables = {
     slurp
     wireplumber
     xdg-desktop-portal-gtk
-    #xdg-dekstop-portal-kde
     xdg-desktop-portal-hyprland
     zsh
     lsd
@@ -429,164 +323,6 @@ environment.sessionVariables = {
     udev-gothic-nf
     noto-fonts
     hyprpicker
-    gnome.gnome-keyring
-    imagemagick
-    pamixer
-    libsForQt5.kdeconnect-kde
-   # kdeconnect-kde
-    xdotool
-    xbindkeys
-    neofetch
-    electron
-    vscodium
-    ntfs3g
-    pkgs.oh-my-zsh
-    pkgs.zsh-completions
-    pkgs.zsh-powerlevel10k
-    pkgs.zsh-syntax-highlighting
-    #pkgs.zsh-history-substring-search
-    sublime
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    font-awesome
-    source-han-sans
-    source-han-sans-japanese
-    source-han-serif-japanese
-    hyprlock
-    nwg-look
-    xbindkeys
-    kdenlive
-    parsec-bin
-    plexamp
-    flatpak
-    pkgs.home-manager
-    todoist-electron
-    masterpdfeditor
-    tailscale
-    actkbd
-    bottles
-    arduino-ide
-    pkgs.polkit_gnome
-    vencord
-    jetbrains-mono
-    libreoffice
-    fastfetch
-    dos2unix
-    
-    #themes
-    colloid-kde
-    colloid-gtk-theme
-    colloid-icon-theme
-    papirus-icon-theme
-
-    #Java
-    maven
-    jdk17
-    speedtest-cli
-    nodejs
-    kwalletmanager
-    nerdfonts
-      
-    #v4l2loopback
-    #privateGPT dependencies
-    python311
-    stdenv.cc.cc.lib
-    libstdcxx5
-  # poetry
-    gnumake
-    cmake
-    ninja
-    gcc
+    gnome.gnome-system-monitor
   ];
-  
-  #postgresql
-  services.postgresql = {
-    enable = true;
-  #package = pkgs.postgresql_15;
-  # ...
-  };
-
-  # ZSH
-  users.defaultUserShell = pkgs.zsh;
-  programs.fish.enable = true;
-  # Insecure packages
-  nixpkgs.config.permittedInsecurePackages = [ "electron-25.9.0" ];
-
-  # XDG desktop portals
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ 
-	pkgs.xdg-desktop-portal-gtk
-	#pkgs.xdg-dekstop-portal-kde
-	 ];
-
-  systemd.user.services.xdg-desktop-portal-gtk = {
-      wantedBy = [ "xdg-desktop-portal.service" ];
-      before = [ "xdg-desktop-portal.service" ];
-   };
-  #KDEConnect
-  programs.kdeconnect.enable = true;
-
-  #GSConnect
-  
-
-
-
-  # OpenGL
-   hardware.opengl = {
-  enable = true;
-  driSupport = true;
-  driSupport32Bit = true;
-  extraPackages = with pkgs; [
-    pkgs.vulkan-loader
-    pkgs.vulkan-validation-layers
-    #pkgs.nvidia-x11.vulkan-driver
-    ];
-  };  
- 
-  #Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-   };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
-
-  # networking
-  networking.networkmanager.enable = true;
-  # Open ports in the firewall.
-   networking.firewall = { 
-    enable = true;
-    allowedTCPPortRanges = [ 
-      { from = 1714; to = 1764; } # KDE Connect
-    ];
-    allowedTCPPorts = [22 4747 47984 47989 47990 48010 5900 8085 ];  
-    allowedUDPPortRanges = [ 
-      { from = 1714; to = 1764; } # KDE Connect
-      { from = 47998; to = 48000; }
-     #{ from = 8000; to = 8010; }
-    ];  
-    allowedUDPPorts = [ 4747 8085 ];
-  }; 
-
-
-  programs.adb.enable = true;
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
 }
--- flake.nix
+++ flake.nix
@@ -1,74 +1,27 @@
-# /etc/nixos/flake.nix
-
 {
-  description = "flake for octavian";
+  description = "Octavian's NixOS Configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
-   # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-   #  hyprland.url = "github:hyprwm/Hyprland";
-    hyprland = {
-      type = "git";
-      url = "https://github.com/hyprwm/Hyprland";
-      submodules = true;
-    }; 
-   
-    stylix.url = "github:danth/stylix";
-    
-    hyprland-plugins = {
-        url = "github:hyprwm/hyprland-plugins";
-        inputs.hyprland.follows = "hyprland"; 
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    
-    home-manager={
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
-      };
-    
-    nix-flatpak.url = "github:gmodena/nix-flatpak";  
-    
-    nix-colors.url = "github:misterio77/nix-colors";
-    
-    };
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
 
-  outputs = { self, nixpkgs,home-manager,nix-flatpak,stylix,... }@inputs:
-  #  let   
-  #    system= "x86_64-linux";
-  #    pkgs = import nixpkgs{
-  #      inherit system;
-  #      config = {
-  #        allowUnfree = true;
-  #      };
-  #    };
-  #   in
+  outputs = { nixpkgs, home-manager, hyprland, ... }: let
+    system = "x86_64-linux";
+  in
   {
-   nixosConfigurations = {
-      octavian=nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs ;};#system
-      system= "x86_64-linux";
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
-          ./dotfiles/default.nix
-          ./apps/default.nix
-          #./home.nix
-          nix-flatpak.nixosModules.nix-flatpak
-          #stylix.nixosModules.stylix
         ];
       };
     };
- 
-
-    # homeConfigurations."octavian" = home-manager.lib.homeManagerConfiguration {
-    #   pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    #   modules = [ stylix.homeManagerModules.stylix ./home.nix ];
-    # };
-  # homeConfigurations."octavian" = home-manager.lib.homeManagerConfiguration {
-  #   pkgs = nixpkgs.legacyPackages.x86_64_linux;
-  #   modules = [
-  #     ./home.nix
-  #     stylix.homeManagerModules.stylix
-  #   ];
-  # };
-};
+  };
 }
