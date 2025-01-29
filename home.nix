e75323c6a2f100a6349f9e2f991cfc91a2a1df75
Octavian <soctavianstefan@gmail.com>
2025-01-29 11:26:18 +0200

    plasma-manager

--- home.nix
+++ home.nix
@@ -4,6 +4,7 @@
   imports = [
       # inputs.nix-colors.homeManagerModules.default
       ./features/alacritty.nix
+      ./modules/plasma.nix
       # ./dotfiles/hyprland.nix
       # ./modules/sunshine.nix
   ];
