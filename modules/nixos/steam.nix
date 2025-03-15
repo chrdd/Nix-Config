481de5ebd639b228e64ef207ae795221b0782845
Octavian <soctavianstefan@gmail.com>
2025-03-15 20:54:33 +0200

    Update +gamescope/itch/gog

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -4,5 +4,8 @@
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server  
+    gamescopeSession.enable = true;
   };
+  programs.steam.extraCompatPackages = [ pkgs.proton-ge-bin];
 }
