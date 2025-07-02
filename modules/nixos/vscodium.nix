b1b719e8edf22ba94eb47a517beef3769858f07b
Octavian <soctavianstefan@gmail.com>
2025-07-02 20:00:00 +0300

    Auto-commit before pull: 2025-07-02T20:00:00+03:00

--- modules/nixos/vscodium.nix
+++ modules/nixos/vscodium.nix
@@ -9,21 +9,29 @@
     nil
     (vscode-with-extensions.override {
       vscode = vscodium;
-      vscodeExtensions = with vscode-extensions; [
-        kamadorueda.alejandra
-        jnoortheen.nix-ide
-        ms-python.python
-        redhat.vscode-yaml
-        ms-python.debugpy
-        # ]
-        # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        #   {
-        #     name = "vscode-theme-onedark";
-        #     publisher = "akamud";
-        #     version = "2.3.0";
-        #     sha256 = "26e661d80c2b656297f570d5522214cd1deee6533b699c8c9dbbaaf5c366fd30";
-        #   }
-      ];
+      vscodeExtensions = with vscode-extensions;
+        [
+          kamadorueda.alejandra
+          jnoortheen.nix-ide
+          ms-python.python
+          redhat.vscode-yaml
+          ms-python.debugpy
+          mhutchie.git-graph
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "vscode-theme-onedark";
+            publisher = "akamud";
+            version = "2.3.0";
+            sha256 = "sha256-8GGv4L4poTYjdkDwZxgNYajuEmIB5XF1mhJMxO2Ho84=";
+          }
+          {
+            name = "theme-monokai-pro-vscode";
+            publisher = "monokai";
+            version = "2.0.7";
+            sha256 = "sha256-MRFOtadoHlUbyRqm5xYmhuw0LL0qc++gR8g0HWnJJRE=";
+          }
+        ];
     })
   ];
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
