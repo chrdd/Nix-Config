7a7004bcfd4110f48820867434917776a049db2f
Octavian <soctavianstefan@gmail.com>
2025-07-02 14:17:58 +0300

    VSCodium

--- modules/nixos/vscodium.nix
+++ modules/nixos/vscodium.nix
@@ -1,18 +1,30 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }: {
   environment.systemPackages = with pkgs; [
+    nixd
+    nil
     (vscode-with-extensions.override {
       vscode = vscodium;
       vscodeExtensions = with vscode-extensions; [
         kamadorueda.alejandra
         jnoortheen.nix-ide
-        akamud.vscode-theme-onedark
         ms-python.python
         redhat.vscode-yaml
+        ms-python.debugpy
+        # ]
+        # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        #   {
+        #     name = "vscode-theme-onedark";
+        #     publisher = "akamud";
+        #     version = "2.3.0";
+        #     sha256 = "26e661d80c2b656297f570d5522214cd1deee6533b699c8c9dbbaaf5c366fd30";
+        #   }
       ];
     })
   ];
+  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 }
