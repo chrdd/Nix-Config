7edee1a4f888042a5e723da3a1588ade6efda25d
Octavian <soctavianstefan@gmail.com>
2025-07-27 14:54:34 +0300

    Update

--- modules/nixos/optimisation.nix
+++ modules/nixos/optimisation.nix
@@ -5,9 +5,9 @@
   # NOTE: Optimizes during builds - results in slower builds
   nix.settings.auto-optimise-store = true;
   # Purge Unused Nix-Store Entries
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
-  };
+  # nix.gc = {
+  #   automatic = true;
+  #   dates = "weekly";
+  #   options = "--delete-older-than 30d";
+  # };
 }
