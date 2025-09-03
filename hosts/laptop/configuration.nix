2e55601c4476635527192aa1a67804682296c65e
Octavian <soctavianstefan@gmail.com>
2025-09-03 14:44:31 +0300

    Auto-commit before pull: 2025-09-03T14:44:31+03:00

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -197,6 +197,7 @@
     localsend
     lsd
     lutris
+    mailspring
     moonlight-qt
     mumble
     nix-output-monitor
@@ -305,28 +306,6 @@
 
   # Powertop
   powerManagement.powertop.enable = true;
-
-  #Fingerprint sensor
-  # # Start the driver at boot
-  # systemd.services.fprintd = {
-  #   wantedBy = ["multi-user.target"];
-  #   serviceConfig.Type = "simple";
-  # };
-
-  # # Install the driver
-  # services.fprintd.enable = true;
-  # # If simply enabling fprintd is not enough, try enabling fprintd.tod...
-  # services.fprintd.tod.enable = true;
-  # # ...and use one of the next four drivers
-  # # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-elan; # Elan(04f3:0c4b) driver
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; # (Marked as broken as of 2025/04/23!) driver for 2016 ThinkPads
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)
-
-  # however for focaltech 2808:a658, use fprintd with overidden package (without tod)
-  # services.fprintd.package = pkgs.fprintd.override {
-  #   libfprint = pkgs.libfprint-focaltech-2808-a658;
-  # };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
