126a86bbda8efe2911b107341bb5d0ba9f06ec6a
Octavian <soctavianstefan@gmail.com>
2025-09-26 08:35:36 +0300

    Auto-commit before pull: 2025-09-26T08:35:36+03:00
    	modified:   modules/nixos/default_Acer.nix
    	modified:   modules/nixos/default_Orion.nix
    	modified:   modules/nixos/tmux.nix
    	modified:   modules/nixos/vscodium.nix

--- modules/nixos/default_Acer.nix
+++ modules/nixos/default_Acer.nix
@@ -32,6 +32,7 @@
     # ./virtualbox.nix
     # ./wireguard.nix
   ];
+<<<<<<< HEAD
   vivaldi.enable = true;
   zsh.enable = true;
   docker.enable = true;
@@ -39,4 +40,16 @@
   autoupgrade.enable = true;
   git.enable = true;
   fonts.enable = true;
+=======
+  vivaldi.enable = lib.mkDefault true;
+  zsh.enable = lib.mkDefault true;
+  docker.enable = lib.mkDefault true;
+  adb.enable = lib.mkDefault true;
+  autoupgrade.enable = lib.mkDefault true;
+  git.enable = lib.mkDefault true;
+  fonts.enable = lib.mkDefault true;
+  nh.enable = lib.mkDefault true;
+  tmux.enable = lib.mkDefault true;
+  vscodium.enable = lib.mkDefault true;
+>>>>>>> cd966dd (Auto-commit before pull: 2025-09-26T08:35:36+03:00)
 }
