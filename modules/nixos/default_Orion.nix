c4c8bb764d9135a04e5ed0c4b24fd666167c508a
Octavian <soctavianstefan@gmail.com>
2025-09-26 08:35:36 +0300

    Auto-commit before pull: 2025-09-26T08:35:36+03:00
    	modified:   modules/nixos/default_Acer.nix
    	modified:   modules/nixos/default_Orion.nix
    	modified:   modules/nixos/tmux.nix
    	modified:   modules/nixos/vscodium.nix

--- modules/nixos/default_Orion.nix
+++ modules/nixos/default_Orion.nix
@@ -32,6 +32,7 @@
     # ./virtualbox.nix
   ];
 
+<<<<<<< HEAD
   vivaldi.enable = true;
   zsh.enable = true;
   docker.enable = true;
@@ -40,4 +41,16 @@
   git.enable = true;
   fonts.enable = true;
   nh.enable = true;
+=======
+  vivaldi.enable = lib.mkDefault true;
+  zsh.enable = lib.mkDefault true;
+  docker.enable = lib.mkDefault true;
+  adb.enable = lib.mkDefault true;
+  autoupgrade.enable = lib.mkDefault true;
+  git.enable = lib.mkDefault true;
+  fonts.enable = lib.mkDefault true;
+  nh.enable = lib.mkDefault true;
+  tmux.enable = lib.mkDefault true;
+  vscodium.enable = lib.mkDefault true;
+>>>>>>> cd966dd (Auto-commit before pull: 2025-09-26T08:35:36+03:00)
 }
--- modules/nixos/vscodium.nix
+++ modules/nixos/vscodium.nix
@@ -4,35 +4,40 @@
   inputs,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
-    nixd
-    nil
-    (vscode-with-extensions.override {
-      vscode = vscodium;
-      vscodeExtensions = with vscode-extensions;
-        [
-          jnoortheen.nix-ide
-          kamadorueda.alejandra
-          mhutchie.git-graph
-          ms-python.debugpy
-          ms-python.python
-          redhat.vscode-yaml
-        ]
-        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "vscode-theme-onedark";
-            publisher = "akamud";
-            version = "2.3.0";
-            sha256 = "sha256-8GGv4L4poTYjdkDwZxgNYajuEmIB5XF1mhJMxO2Ho84=";
-          }
-          {
-            name = "theme-monokai-pro-vscode";
-            publisher = "monokai";
-            version = "2.0.7";
-            sha256 = "sha256-MRFOtadoHlUbyRqm5xYmhuw0LL0qc++gR8g0HWnJJRE=";
-          }
-        ];
-    })
-  ];
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+  options = {
+    vscodium.enable = lib.mkEnableOption "Enables VsCodium";
+  };
+  config = lib.mkIf config.vscodium.enable {
+    environment.systemPackages = with pkgs; [
+      nixd
+      nil
+      (vscode-with-extensions.override {
+        vscode = vscodium;
+        vscodeExtensions = with vscode-extensions;
+          [
+            jnoortheen.nix-ide
+            kamadorueda.alejandra
+            mhutchie.git-graph
+            ms-python.debugpy
+            ms-python.python
+            redhat.vscode-yaml
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "vscode-theme-onedark";
+              publisher = "akamud";
+              version = "2.3.0";
+              sha256 = "sha256-8GGv4L4poTYjdkDwZxgNYajuEmIB5XF1mhJMxO2Ho84=";
+            }
+            {
+              name = "theme-monokai-pro-vscode";
+              publisher = "monokai";
+              version = "2.0.7";
+              sha256 = "sha256-MRFOtadoHlUbyRqm5xYmhuw0LL0qc++gR8g0HWnJJRE=";
+            }
+          ];
+      })
+    ];
+    nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+  };
 }
