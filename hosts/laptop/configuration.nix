803dcb82686b76a986adb8332c61df689a47edba
Octavian <soctavianstefan@gmail.com>
2026-02-19 10:00:02 +0200

    Auto-commit before pull: 2026-02-19T10:00:02+02:00

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   inputs,
@@ -22,15 +19,10 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  #   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
 
   # Enable networking
-  #   networking.networkmanager.enable = true;
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Bucharest";
@@ -75,18 +67,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.octavian = {
     isNormalUser = true;
     description = "octavian";
@@ -103,20 +86,13 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # floorp-bin
     # nh
     # prismlauncher
     # rustdesk
     # tigervnc
-    python315
-    steam-run
-    splix
-    scrcpy
-    zapzap
-    powershell
     alacritty
     anki
     anydesk
@@ -128,7 +104,6 @@
     fastfetch
     ffmpeg
     freecad
-    # floorp-bin
     gh
     ghostty
     git
@@ -147,13 +122,20 @@
     nixd
     obs-studio
     obsidian
+    openssl
     plex-desktop
     plexamp
+    powershell
+    python315
     qbittorrent-enhanced
+    scrcpy
     smartmontools
+    splix
+    steam-run
     syncthing
     tailscale
     thunderbird
+    ticktick
     tmux
     todoist-electron
     unrar
@@ -163,27 +145,19 @@
     wget
     wireguard-tools
     wireguard-ui
+    zapzap
     zoxide
-    ticktick
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-  networking.hostName = "Acer"; # Define your hostname.
+  networking.hostName = "Acer";
 
   programs.kdeconnect.enable = true;
-  # Enable Hyprland
-  #   programs.hyprland = {
-  #     enable = true;
-  #     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-  #     xwayland.enable = true;
-  #   };
 
   nix.settings.require-sigs = false;
 
@@ -193,11 +167,5 @@
   # Powertop
   powerManagement.powertop.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 }
