aea7ea1427f0f65bf5384e3f9f3248270bf46226
Octavian <soctavianstefan@gmail.com>
2026-02-20 17:00:11 +0200

    Auto-commit before pull: 2026-02-20T17:00:11+02:00

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -4,64 +4,41 @@
   lib,
   ...
 }: {
-  age.secrets.wg-key-vps = {
-    file = "${inputs.self.outPath}/secrets/wg-key-vps.age";
-    # for permission, see man systemd.netdev
-    mode = "640";
-    owner = "systemd-network";
-    group = "systemd-network";
+  networking.firewall = {
+    allowedUDPPorts = [51820];
   };
+  # Enable WireGuard
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the client's end of the tunnel interface.
+      ips = ["10.4.7.104/24"];
+      # listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
 
-  networking.firewall.allowedUDPPorts = [51820];
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "/home/octavian/wireguard-keys/privatekey";
 
-  networking.useNetworkd = true;
+      peers = [
+        # For a client configuration, one peer entry for the server will suffice.
 
-  systemd.network = {
-    enable = true;
-
-    networks."50-wg0" = {
-      matchConfig.Name = "wg0";
-
-      address = [
-        # /32 and /128 specifies a single address
-        # for use on this wg peer machine
-        "fd31:bf08:57cb::7/128"
-        "192.168.26.7/32"
-      ];
-    };
-
-    netdevs."50-wg0" = {
-      netdevConfig = {
-        Kind = "wireguard";
-        Name = "wg0";
-      };
-
-      wireguardConfig = {
-        ListenPort = 51820;
-
-        PrivateKeyFile = config.age.secrets.wg-key-vps.path;
-
-        # To automatically create routes for everything in AllowedIPs,
-        # add RouteTable=main
-        RouteTable = "main";
-
-        # FirewallMark marks all packets send and received by wg0
-        # with the number 42, which can be used to define policy rules on these packets.
-        FirewallMark = 42;
-      };
-      wireguardPeers = [
         {
-          # laptop wg conf
-          PublicKey = "ronr+8v670J0CPb0xT5QLGMWDfE7+1g7HmC6YMdCIDk=";
-          AllowedIPs = [
-            "fd31:bf08:57cb::9/128"
-            "192.168.26.9/32"
-          ];
-          Endpoint = "192.168.1.26:51820";
+          # Public key of the server (not a file path).
+          publicKey = "vs9f/MndTXJHlElbDxIMMx6j8T3h1vKJFXvq8Esm7Sw=";
 
-          # RouteTable can also be set in wireguardPeers
-          # RouteTable in wireguardConfig will then be ignored.
-          # RouteTable = 1000;
+          # Forward all the traffic via VPN.
+          allowedIPs = ["10.4.7.0/24" "10.4.0.0/16" "5.2.139.5/32" "5.2.145.42/32" "5.2.164.255/32" "5.2.178.142/32" "5.2.200.31/32" "5.2.205.50/32" "5.2.208.69/32" "78.31.59.10/32" "81.12.137.82/32" "81.196.20.78/32" "82.76.14.195/32" "82.77.25.100/32" "82.76.208.130/32" "82.78.126.4/32" "82.79.139.98/32" "82.208.177.236/32" "83.103.212.20/32" "84.247.72.82/32" "85.186.25.103/32" "86.34.180.234/32" "86.105.108.254/32" "86.124.19.0/32" "86.125.219.237/32" "89.121.242.250/32" "92.55.145.98/32" "92.86.31.182/32" "109.100.121.34/32" "109.103.139.130/32" "109.166.183.30/32" "109.166.232.53/32" "213.177.31.164/32" "217.73.170.74/32"];
+          # Or forward only particular subnets
+          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
+
+          # Set this to the server IP and port.
+          endpoint = "148.251.253.27:51827"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+
+          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          persistentKeepalive = 25;
         }
       ];
     };
